5e54b4b90f312b63703c6476c26a0d4fb3ad2a5e
Troseberry <tayroseberry@gmail.com>
2021-12-12 23:32:43 -0800

    Create GetAllFanficsOnPage.js

---
+++ netlify/functions/GetAllFanficsOnPage.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const svc = require('../services');
+
+exports.handler = async function(event, context) {
+    // your server-side functionality
+
+    const pageNumber = event.queryStringParameters.pageNumber;
+    const tag = svc.encodeTagForUrl(event.queryStringParameters.tagName);
+    const pageUrl = `http://archiveofourown.org/tags/${tag}/works?page=${pageNumber}`;
+    
+    let fics = [];
+    fics = await svc.scrapeFanFicsOnPage(pageUrl);
+
+    //res.send(fics);
+    return {
+        statusCode: 200,
+        body: fics
+    }
+}
