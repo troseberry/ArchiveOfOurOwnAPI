1e170fc5073946bf0bb3bf1f31ee8e4cc5ce7690
Troseberry <tayroseberry@gmail.com>
2021-12-12 23:54:12 -0800

    Update GetAllFanficsOnPage.js

--- netlify/functions/GetAllFanficsOnPage.js
+++ netlify/functions/GetAllFanficsOnPage.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const svc = require('../services');
+const svc = require('/api/services');
 
 exports.handler = async function(event, context) {
     // your server-side functionality
